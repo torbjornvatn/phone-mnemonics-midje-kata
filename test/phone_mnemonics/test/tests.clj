bf832947bdfcfbca6490d8b4b7ef4219f63755db
Torbjørn Vatn <torbjorn@vatn.org>
2011-09-18 22:04:31 +0200

    Adds a fact about number of letters

--- test/phone_mnemonics/test/tests.clj
+++ test/phone_mnemonics/test/tests.clj
@@ -1,6 +1,7 @@
 (ns phone-mnemonics.test.tests
   (:use [phone-mnemonics.core])
-  (:use [midje.sweet]))
+  (:use [midje.sweet])
+  (:require [clojure.string :as str]))
 
 (unfinished words)
 
@@ -16,6 +17,8 @@
 (defn encode[number]
   #{(first (words))})
 
+(defn words-for-num[] (set (filter #(<= (count %) 8) (words))))
+
 (fact "A number should be encoded to one or more matching words"
   (encode ...number...) => #{...word...}
   (provided
@@ -24,3 +27,7 @@
 (fact "A map of mnemonics must be available"
   mnemonics => (contains {\2 "ABC"}))
 
+(fact "We only want words that are 8 letters or shorter"
+  (words-for-num) => #{"short" "medium" "8letters"}
+  (provided
+    (words) => #{"short" "medium" "8letters" "10letters"}))
