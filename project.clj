c980abeccd2988de74728fdead118a10da0692e0
Torbjørn Vatn <torbjorn@vatn.org>
2011-09-18 22:03:54 +0200

    Adds clojure/contrib

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject phone-mnemonics "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"] [midje "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"] [midje "1.2.0"] [org.clojure/clojure-contrib "1.2.0"]])
